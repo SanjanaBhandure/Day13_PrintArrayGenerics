04430c31808a1c8ea5ec2687452be5a1b5d4799b
SanjanaBhandure <sweetybhandure@gmail.com>
2022-05-01 13:18:05 +0530

    Added: Printing int, double, and char array in generic way

--- PrintArray.java
+++ PrintArray.java
@@ -1,45 +1,25 @@
 /*
- * Given an array of Integer, Double and Character, write a program to print the same -
- * Create PrintArray class and define toPrint method to print corresponding elements to stdout
+ * Given an array of Integer, Double and Character, write a program to print the UC 2 same using Generics
  *
  * @author: Sanjana Bhandure
- * @version: 1.0
+ * @version: 1.2
  * @date: 1-5-2022
  */
+
 package com.Bridgelabz.Day13PrintArrayGenerics;
 
 public class PrintArray {
-    /*
-     * printArray method created for printing integer array
-     */
-    static public void printArray(int[] arr) {
-        for (int element : arr)
-            System.out.print(element + " ");
-        System.out.println();
-    }
-
-    /*
-     * printArray method created for printing float/double array
-     */
-    static public void printArray(double[] arr) {
-        for (Double element : arr)
-            System.out.print(element + " ");
-        System.out.println();
-    }
-
-    /*
-     * printArray method created for printing string array
-     */
-    static public void printArray(char[] arr) {
-        for (char element : arr)
+    // generic array printing
+    static public <T> void printArray(T[] arr) {
+        for (T element : arr)
             System.out.print(element + " ");
         System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr1 = { 10, 20, 30 };
-        double[] arr2 = { 1.23, 2.24, 3.345 };
-        char[] arr3 = { 'a', 'A', 'V' };
+        Integer[] arr1 = { 10, 20, 30 };
+        Double[] arr2 = { 1.23, 2.24, 3.345 };
+        Character[] arr3 = { 'a', 'A', 'V' };
         printArray(arr1);
         printArray(arr2);
         printArray(arr3);
